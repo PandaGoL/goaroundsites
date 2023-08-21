d63f63c54972bc8a080d762b1f85a1ea7499da99
Fursov Petr <p.fursov@qvant.ru>
2023-08-21 17:38:08 +0300

    быстрая обработка запросов к сайтам

---
+++ src/src.go
@@ -0,0 +1,126 @@
+package src
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sync/errgroup"
+)
+
+/*
+Сегодня мы будем обходить заданные сайты и собирать в хэштаблицу результаты обхода: имя сайма, статус ответа, время ответа.
+Если данные уже есть в хэштаблице - обновим их.
+Ходить по сети мы будем раз в requestFrequency после предыдущего похода, печатать значения раз в секунду
+(можете выбрать, другой промежуток).
+Вам понадобятся мьютексы для безопасного доступа к хэштаблице.
+Так же нужно корректно использоваться контекст, чтобы завершать работу функций при его отмене.
+*/
+
+const (
+	// you can limit concurrent net request. It's optional
+	MaxGoroutines = 1
+	// timeout for net requests
+	Timeout = 2 * time.Second
+)
+
+type SiteStatus struct {
+	Name          string
+	StatusCode    int
+	TimeOfRequest time.Time
+}
+
+type Monitor struct {
+	StatusMap        map[string]SiteStatus
+	Mtx              *sync.Mutex
+	G                errgroup.Group
+	Sites            []string
+	RequestFrequency time.Duration
+}
+
+func NewMonitor(sites []string, requestFrequency time.Duration) *Monitor {
+	return &Monitor{
+		StatusMap:        make(map[string]SiteStatus),
+		Mtx:              &sync.Mutex{},
+		Sites:            sites,
+		RequestFrequency: requestFrequency,
+	}
+}
+
+func (m *Monitor) Run(ctx context.Context) error {
+	// run printStatuses and checkSite in different goroutines
+	m.G.Go(func() error {
+		return m.printStatuses(ctx)
+	})
+
+	for _, site := range m.Sites {
+		site := site
+		m.G.Go(func() error {
+			ticker := time.NewTicker(m.RequestFrequency)
+			defer ticker.Stop()
+
+			for {
+				select {
+				case <-ctx.Done():
+					return ctx.Err()
+				case <-ticker.C:
+					m.checkSite(ctx, site)
+				}
+			}
+		})
+	}
+
+	if err := m.G.Wait(); err != nil && err != context.Canceled {
+		return err
+	}
+	return nil
+}
+
+func (m *Monitor) checkSite(ctx context.Context, site string) {
+	// check site and write result to StatusMap
+
+	client := http.Client{
+		Timeout: Timeout,
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, site, nil)
+	if err != nil {
+		logrus.Errorf("Bad request: %s", err)
+		return
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		logrus.Errorf("Cant do request: %s", err)
+		return
+	}
+
+	m.Mtx.Lock()
+	m.StatusMap[site] = SiteStatus{
+		Name:          site,
+		StatusCode:    resp.StatusCode,
+		TimeOfRequest: time.Now(),
+	}
+	m.Mtx.Unlock()
+}
+
+func (m *Monitor) printStatuses(ctx context.Context) error {
+	ticker := time.NewTicker(time.Second * 1)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-ticker.C:
+			m.Mtx.Lock()
+			for _, status := range m.StatusMap {
+				fmt.Printf("%s, %d, %v\n", status.Name, status.StatusCode, status.TimeOfRequest)
+			}
+			m.Mtx.Unlock()
+		}
+	}
+}
